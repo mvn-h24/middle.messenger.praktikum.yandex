0eb0e099664c5ecbf901b6854b86b721595475c0
lowRideSky <morning.vecher.night@yandex.ru>
2023-06-09 07:37:11 +0300

    test

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ import {resolve} from 'node:path';
 const PORT = 3000;
 const saticDirPath = './dist';
 const distPath = resolve(fileURLToPath(new URL('.', import.meta.url)), saticDirPath);
-const appEntryPoint = resolve(distPath, 'index.html');
+const appEntryPoint = resolve('dist', 'index.html');
 
 const app = express();
-app.use(express.static(distPath));
-app.all('*', (_, res) => res.sendFile(appEntryPoint));
+app.use(express.static(saticDirPath));
+app.get('*', (_, res) => res.sendFile(appEntryPoint));
 app.listen(PORT);
