dea8f644f69459850bcc395c114a01eda591ef8a
lowRideSky <morning.vecher.night@yandex.ru>
2023-06-10 18:26:41 +0300

    feat(style): базовая стилизация

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,9 @@
 import {render} from "lit-html";
 import {createRouter} from "./router";
+import './styles/app.css';
 
 export const createApp = () => {
     const router = createRouter();
-
     return {
         render: (mountPoint) => render(router.currentPageTemplate, mountPoint)
     }
