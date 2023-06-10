993f82d7cf6a0b4c4586861f12f022c0a487f611
lowRideSky <morning.vecher.night@yandex.ru>
2023-06-10 08:52:09 +0300

    chore: добавляет vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
  import {defineConfig} from "vite";
 import {resolve} from "node:path";
+ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@/shared': resolve(__dirname, './src/shared'),
-            '@/app': resolve(__dirname, './src/app'),
-            '@/pages': resolve(__dirname, './src/pages'),
-        }
-    }
+    plugins: [tsconfigPaths()],
 });
