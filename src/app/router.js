f1af827a8b4f2c89e0f1be86f934b633afc0fdd1
lowRideSky <morning.vecher.night@yandex.ru>
2023-06-11 18:54:54 +0300

    refactor(app): страницы
    Удаляет страницу  загрузки аватара

--- src/app/router.js
+++ src/app/router.js
@@ -4,10 +4,9 @@ import pageNotFound from '@/pages/404';
 import serverErrorPage from '@/pages/500';
 import loginPage from '@/pages/login';
 import signinPage from '@/pages/signin';
-import profileIndex from '@/pages/profile/index';
+import profileIndex from '@/pages/profile/view';
 import profileEdit from '@/pages/profile/edit';
 import profileChangePassword from '@/pages/profile/change-password';
-import profileUploadAvatar from '@/pages/profile/upload-avatar';
 import chatDetail from '@/pages/chat/[id]';
 
 export const createRouter = () => new Router({
@@ -20,7 +19,6 @@ export const createRouter = () => new Router({
         '/profile/': profileIndex,
         '/profile/edit/':profileEdit,
         '/profile/change-password/':profileChangePassword,
-        '/profile/upload-avatar/': profileUploadAvatar,
         '/chat/detail/': chatDetail,
     }
 });
