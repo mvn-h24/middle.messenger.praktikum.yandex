7dbaf2b78188c55b2c56b212277f7535512d75c8
lowRideSky <morning.vecher.night@yandex.ru>
2023-06-11 19:33:38 +0300

    feat(app): страницы
    Переделывает модуль profile/index.js в profile/view

---
+++ src/pages/profile/view/index.js
@@ -0,0 +1,28 @@
+import {html} from "lit-html";
+import {CenterizedLayout} from "@/shared/ui/CenterizedLayout";
+import {InfoList} from "@/shared/ui/InfoList";
+import {ProfileImage} from "@/entity/user/ui/ProfileImage";
+import {TestProfile} from "@/entity/user/fixture/test-profile.js";
+import cn from './styles.module.css';
+
+const profileList = InfoList({
+    list: TestProfile.info,
+    keyGetter: (item) => item['key'],
+    contentGetter: (item) => item['value'],
+    classes: {
+        [cn.profileView__infoList]:true
+    }
+});
+
+const slot = html`
+    <div class="${cn.profileView}">
+        ${ProfileImage({name: TestProfile.name})}
+        ${profileList}
+        <div class="${cn.profileView__controlList}">
+            <div class="listItem"><a class="${cn.profileView__link} appLink" href="/profile/edit/">Изменить данные</a></div>
+            <div class="listItem"><a class="${cn.profileView__link} appLink" href="/profile/change-password/">Изменить пароль</a></div>
+            <div class="listItem"><a class="${cn.profileView__link} appLink appLink--warning" href="/login/">Выйти</a></div>
+        </div>
+    </div>
+`;
+export default () => CenterizedLayout({slot});
