12dee17865d28767be55ed88a84163eba457ccad
lowRideSky <morning.vecher.night@yandex.ru>
2023-06-12 14:42:35 +0300

    chore(app): code cleanup

--- src/entity/chat/ui/ChatList/index.js
+++ src/entity/chat/ui/ChatList/index.js
@@ -4,20 +4,16 @@ import {map} from "lit-html/directives/map.js";
 import {ChatPreview} from "@/entity/chat/ui/ChatPreview";
 import cn from './styles.module.css';
 
-export const ChatList = ({chatList}) => html`
-    <ul class=${cn.list}>${
-    when(
-        Array.isArray(chatList) && chatList.length,
-        () => map(
-            chatList,
-            (chatMeta) =>
-                html`
-                                <li class=${cn.list__item}>${ChatPreview({chatMeta})}</li>`
-        ),
-        () => html`
-                        <li class=${cn.list__item}>
-                            <div>Ничего не найдено...</div>
-                        </li>`
-    )
-}
+const chatListItem = (chatMeta) => html`
+    <li class=${cn.list__item}>${ChatPreview({chatMeta})}</li>`;
+const emptyListItem = () => html`
+    <li class=${cn.list__item}>
+        <div>Ничего не найдено...</div>
+    </li>`;
+export const ChatList = ({chatList, selectedChat}) => html`
+    <ul class=${cn.list}>${when(
+            Array.isArray(chatList) && chatList.length,
+            () => map(chatList, (chatMeta) => chatListItem(chatMeta)),
+            () => emptyListItem()
+    )}
     </ul>`;
