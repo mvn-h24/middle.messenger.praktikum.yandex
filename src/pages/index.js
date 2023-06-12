aca3fbc5e974a96aba027326d568d625d00a0daa
lowRideSky <morning.vecher.night@yandex.ru>
2023-06-12 11:20:48 +0300

    feat(app): страницы
    Добавляет настраивает страницу листинга чатов

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,5 @@
-import {html} from "lit-html";
+import {ChatListLayout} from "@/entity/chat/ui/ChatListLayout";
+import {ChatFeed as feed} from "@/entity/chat/ui/ChatFeed";
+import {Chat as chat} from "@/entity/chat/ui/Chat";
 
-export default () => {
-    return html`
-        <div>
-            <b>chat app</b>
-            <ul>
-                <li><a href="/404/">404</a></li>
-                <li><a href="/500/">500</a></li>
-                <li><a href="/login/">log in</a></li>
-                <li><a href="/signin/">sign in</a></li>
-                <li><a href="/profile/">profile</a></li>
-                <li><a href="/profile/edit/">profile edit</a></li>
-                <li><a href="/profile/change-password/">change password</a></li>
-                <li><a href="/profile/upload-avatar/">upload avatar</a></li>
-                <li><a href="/chat/detail/">chat details</a></li>
-            </ul>
-        </div>
-    `
-};
+export default () => ChatListLayout({feed, chat});
