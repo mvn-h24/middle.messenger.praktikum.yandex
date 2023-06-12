babe55ea0a74b4b918fb7cc1f7180223e423de97
lowRideSky <morning.vecher.night@yandex.ru>
2023-06-12 18:01:07 +0300

    feat(user-ui): компоненты
    Добавляет кнопку назад

--- src/entity/user/ui/ProfileLayout/index.js
+++ src/entity/user/ui/ProfileLayout/index.js
@@ -6,7 +6,10 @@ import cn from './styles.module.css';
 export const ProfileLayout = ({profile, slot, showName = true}) => {
     const containment = useSlot(slot);
     return html`
-        <div class="${cn.profileLayout}">
+        <div class=${cn.profileLayout}>
+            <div class=${cn.profile__pageControlPanel}>
+                <a class="appBtn appBtn_icon appBtn_iconArrowLeft"  href="/"></a>
+            </div>
             ${ProfileImageModal({name: showName ? profile.name : null})}
             ${containment}
         </div>
